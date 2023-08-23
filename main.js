7d089c80a3f1cd4c6665d02d13fda3bd7bee093b
Paige <paigeberliner@gmail.com>
2023-08-23 13:48:11 -0400

    Complete Lab

--- main.js
+++ main.js
@@ -3,6 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelectorAll(".like-glyph").forEach(glyph => {
+  glyph.addEventListener('click', simpleLiker);
+})
+
+function simpleLiker (){
+mimicServerCall()
+.then(res => {
+  document.querySelectorAll(".like-glyph").forEach(glyph => {
+    glyph.addEventListener('click', () => {
+      glyph.textContent = FULL_HEART;
+    });
+    document.querySelectorAll(".like-glyph").forEach(glyph => {
+      glyph.addEventListener('click', () => {
+        if (glyph.classList.contains("activated-heart")) {
+          glyph.textContent = EMPTY_HEART;
+          glyph.classList.remove("activated-heart");
+      } else {
+      glyph.textContent = FULL_HEART;
+      glyph.classList.add("activated-heart");
+         }
+      });
+    });
+  });
+})
+
+.catch(function(error){
+  console.log(error)
+  let errorElement = document.querySelector("#modal");
+  errorElement.classList.remove("hidden")
+  setTimeout(function() { 
+  errorElement.classList.add("hidden")
+  }, 3000)
+  document.querySelector('#modal-message').innerHTML=error
+});
+}
+
+
+
+
 
 
 
